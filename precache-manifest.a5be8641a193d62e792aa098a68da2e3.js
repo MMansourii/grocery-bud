eb86dbe941b980a8ddc1601ca24613fc5eded5ba
Mohammad <57011800+Mammad10@users.noreply.github.com>
2021-07-04 10:23:44 +0300

    Updates

---
+++ precache-manifest.a5be8641a193d62e792aa098a68da2e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed785081f6f9be857dd8595d4d924460",
+    "url": "/grocery-bud/index.html"
+  },
+  {
+    "revision": "e361767aa4e38769143a",
+    "url": "/grocery-bud/static/css/main.a903c182.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/grocery-bud/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grocery-bud/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e361767aa4e38769143a",
+    "url": "/grocery-bud/static/js/main.a194b5f2.chunk.js"
+  },
+  {
+    "revision": "f19ce6e521be50b493db",
+    "url": "/grocery-bud/static/js/runtime-main.8b040e9c.js"
+  }
+]);
